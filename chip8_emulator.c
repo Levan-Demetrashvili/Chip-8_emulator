43b7d91d8672bb474270ab1ae65d612c89e4d065
Levan-Demetrashvili <levandemetrashvili10@gmail.com>
2026-09-16 16:44:04 +0400

    Implement ALU and examine infinite loop problem

--- chip8_emulator.c
+++ chip8_emulator.c
@@ -1,7 +1,8 @@
-#include <cs50.h>
+// #include <cs50.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
@@ -15,7 +16,7 @@ typedef struct {
 	uint8_t sound_timer;
 	uint8_t screen[32][64];
 	bool keypad[16];
-	bool shift_quirk;
+	bool legacy_version;
 } CHIP8;
 
 const uint8_t FONTSET[80] = {
@@ -68,7 +69,7 @@ int main(int argc, char *argv[]) {
 	chip8.pc = 0x200;
 
 	while (true) {
-		if (chip8.pc > 1000) {
+		if (chip8.pc > 0xFFF) {
 			printf("Out of bound error");
 			return 3;
 		}
@@ -87,106 +88,206 @@ void emulate_cycle(CHIP8 *chip8, uint16_t opcode) {
 	uint8_t n = opcode & 0x000F;
 	uint16_t nnn = opcode & 0x0FFF;
 
-	switch (opcode & 0xF000) {
-	case 0x0000:
-		switch (opcode & 0xF00F) {
-		case 0x00E0:
-			printf("0x0__0\n");
-			memset(chip8->screen, 0, sizeof(chip8->screen));
-			break;
-		case 0x00EE:
-			printf("0x0__E\n");
-			chip8->sp -= 1;
-			chip8->pc = chip8->stack[chip8->sp];
-			break;
+    switch (opcode & 0xF000) {
+        case 0x0000:
+            switch (opcode) {
+                case 0x00E0:
+                    printf("0x0__0\n");
+                    memset(chip8->screen, 0, sizeof(chip8->screen));
+                    break;
+                case 0x00EE:
+                    printf("0x0__E\n");
+                        chip8->sp -= 1;
+                        chip8->pc = chip8->stack[chip8->sp];
+                    break;
 
-		default:
-			break;
-		}
-    
-    case 0x1000:
-        printf("0x1\n");
-        chip8->pc = nnn;
-        break;
-    case 0x2000:
-        printf("0x2\n");
-        chip8->stack[chip8->sp] = chip8->pc; 
-        chip8->sp += 1;
-        chip8->pc = nnn;
-        break;
+                default:
+                    printf("UNHANDLED OPCODE: 0x%04X at PC: 0x%03X\n", opcode, chip8->pc - 2);
+                    break;
+            }
+            break;
         
-    case 0x3000:
-        printf("0x3\n");
-        if (chip8->registers[x] == kk) {
-            chip8->pc += 2;
+        case 0x1000:
+            printf("0x1 %i %i\n", chip8->pc, nnn);
+            chip8->pc = nnn;
+            break;
+        case 0x2000:
+            printf("0x2 %i %i\n", chip8->pc, nnn);
+            chip8->stack[chip8->sp] = chip8->pc; 
+            chip8->sp += 1;
+            chip8->pc = nnn;
+            break;
+            
+        case 0x3000:
+            printf("0x3\n");
+            if (chip8->registers[x] == kk) chip8->pc += 2;
+            break;
+        case 0x4000:
+            printf("0x4\n");
+            if (chip8->registers[x] != kk) chip8->pc += 2;
+            break;
+        case 0x5000:
+            printf("0x5\n");
+            if (chip8->registers[x] == chip8->registers[y]) chip8->pc += 2;
+            break;
+
+        case 0x6000:
+            printf("0x6: \n");
+            chip8->registers[x] = kk;
+            break;
+        
+        case 0x7000:
+            printf("0x7: \n");
+            chip8->registers[x] += kk;
+            break;
+        case 0x8000:
+
+            switch (opcode & 0xF00F) {
+                case 0x8000:
+                    printf("0x8__0\n");
+                    chip8->registers[x] = chip8->registers[y];
+                    break;
+
+                case 0x8001:
+                    printf("0x8__1\n");
+                    if (chip8->legacy_version) chip8->registers[0xF] = 0;
+                    chip8->registers[x] |= chip8->registers[y];
+                    break;
+
+                case 0x8002:
+                    printf("0x8__2\n");
+                    if (chip8->legacy_version) chip8->registers[0xF] = 0;
+                    chip8->registers[x] &= chip8->registers[y];
+                    break;
+
+                case 0x8003:
+                    printf("0x8__3\n");
+                    if (chip8->legacy_version) chip8->registers[0xF] = 0;
+                    chip8->registers[x] ^= chip8->registers[y];
+                    break;
+
+                case 0x8004:
+                    printf("0x8__4\n");
+                    int sum = chip8->registers[x] + chip8->registers[y];
+                    chip8->registers[x] = sum;
+                    chip8->registers[0xF] =  sum > 255 ? 1 : 0;
+                
+                    break;
+
+                case 0x8005:
+                    printf("0x8__5\n");
+                    chip8->registers[0xF] = chip8->registers[x] >= chip8->registers[y] ? 1 : 0;
+                    chip8->registers[x] -= chip8->registers[y];
+                    break;
+
+                case 0x8006:
+                    printf("0x8__6\n");
+                    if (chip8->legacy_version) {
+                        // Old Systems
+                        chip8->registers[0xF] = chip8->registers[y] & 0x0001;
+                        chip8->registers[x] = chip8->registers[y] >> 1;
+
+                    } else {
+                        // Newer Systems
+                        chip8->registers[0xF] = chip8->registers[x] & 0x0001;
+                        chip8->registers[x] = chip8->registers[x] >> 1;
+                    }
+                    break;
+
+                case 0x8007:
+                    printf("0x8__7\n");
+                    chip8->registers[0xF] = chip8->registers[y] >= chip8->registers[x] ? 1 : 0;
+                    chip8->registers[x] = chip8->registers[y] - chip8->registers[x];
+                    break;
+
+                case 0x800E:
+                    printf("0x8__E\n");
+                    if (chip8->legacy_version) {
+                        // Old Systems
+                        chip8->registers[0xF] = (chip8->registers[y] & 0x80) >> 7;
+                        chip8->registers[x] = chip8->registers[y] << 1;
+
+                    } else {
+                        // Newer Systems
+                        chip8->registers[0xF] = (chip8->registers[x] & 0x80) >> 7;
+                        chip8->registers[x] = chip8->registers[x] << 1;
+                    }
+                    break;
+
+                default:
+                    printf("UNHANDLED OPCODE: 0x%04X at PC: 0x%03X\n", opcode, chip8->pc - 2);
+                    break;
+            }
+            break;
+        case 0x9000:
+            if (chip8->registers[x] != chip8->registers[y]) chip8->pc += 2;
+            break;
+        case 0xA000:
+            printf("0xA\n");
+            chip8->I = nnn;
+            break;
+
+        case 0xB000:
+            printf("0xB\n");
+            if (chip8->legacy_version) {
+                // Older Systems
+                chip8->pc = nnn + chip8->registers[0];
+            }  else {
+                // Newer Systems
+                chip8->pc = nnn + chip8->registers[x];
+            }
+            break;
+        case 0xC000:
+            printf("0xC\n");
+            chip8->registers[x] = (rand() % 256) & kk;
+            break;
+
+        case 0xE000:
+            switch (opcode & 0xF0FF) {
+                case 0xE09E:
+                    printf("0x0xE_9E\n");
+                    uint8_t reg_x1 = chip8->registers[x];    
+                    if (reg_x1 >= 0 && reg_x1 <= 15 && chip8->keypad[reg_x1]) {
+                        chip8->pc += 2;
+                    }
+                    break;
+
+                case 0xE0A1:
+                    printf("0x0xE_A1\n");
+                    uint8_t reg_x2 = chip8->registers[x];    
+                    if (reg_x2 >= 0 && reg_x2 <= 15 && !chip8->keypad[reg_x2]) {
+                        chip8->pc += 2;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+
+        case 0xF000:
+            switch (opcode & 0xF0FF) {
+                case 0xF007:
+                    printf("0xF_07\n");
+                    chip8->registers[x] = 0; 
+                    break;
+
+                default:
+                    printf("UNHANDLED OPCODE: 0x%04X at PC: 0x%03X\n", opcode, chip8->pc - 2);
+                    break;
+            }
+            break;
+        
+        default:
+            printf("UNHANDLED OPCODE: 0x%04X at PC: 0x%03X\n", opcode, chip8->pc - 2);
+            break;
         }
-        break;
-    case 0x4000:
-        printf("0x4\n");
-        if (chip8->registers[x] != kk) {
-            chip8->pc += 2;
-        }
-        break;
-	case 0x5000:
-		printf("0x5\n");
-		if (chip8->registers[x] == chip8->registers[y]) {
-			chip8->pc += 2;
-		}
-		break;
-
-	case 0x6000:
-		printf("0x6: \n");
-		uint8_t value = opcode & 0x00FF;
-		uint8_t register_addr = (opcode & 0x0F00) >> 8;
-		chip8->registers[register_addr] = value;
-		break;
-    
-    case 0x7000:
-        printf("0x7: \n");
-        chip8->registers[x] += kk;
-        break;
-	case 0x8000:
-
-		switch (opcode & 0xF00F) {
-
-		case 0x8006:
-			printf("0x8__6\n");
-			if (chip8->shift_quirk) {
-				// Old Systems
-				chip8->registers[0xf] = chip8->registers[y] & 0x0001;
-				chip8->registers[x] = chip8->registers[y] >> 1;
-
-			} else {
-				// Newer Systems
-				chip8->registers[0xf] = chip8->registers[x] & 0x0001;
-				chip8->registers[x] = chip8->registers[x] >> 1;
-			}
-			break;
-
-		default:
-			// printf("Undefined opcode\n");
-			break;
-		}
-    case 0xF000:
-        switch (opcode & 0xF0FF) {
-            case 0xf007:
-                printf("0xf007\n");
-                chip8->registers[x] = 0; 
-                break;
-            default:
-	        	// printf("Undefined opcode\n");
-                break;
-        }
-	default:
-		// printf("Undefined opcode\n");
-		break;
-	}
 }
 
 void chip8_init(CHIP8 *chip8) {
 	memset(chip8, 0, sizeof(CHIP8));
 	chip8->pc = 0x200;
-	chip8->shift_quirk = false;
+	chip8->legacy_version = false;
 
 	for (int i = 0; i < 80; i++) {
 		chip8->memory[0x50 + i] = FONTSET[i];
